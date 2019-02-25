89e887f4dc9c15f1d128078385e7233dd7b9f664
rodwalker <walker@gar-nb-etp10.poo.doo>
2019-02-25 22:14:29 +0100

    Clean

--- tape.go
+++ tape.go
@@ -22,7 +22,6 @@ func (t *Tape) addFile(f File) {
 	// OR assume we do not mix read and write, so position is the end 
 
 	tf := TapeFile{t.position ,t.position+f.size}
-//	t.catalog = append(t.catalog, tf)
 	t.catalog[f.fileName] = tf
 	t.position = tf.endMB
 }
@@ -43,15 +42,6 @@ type TapeFile struct {
 	startMB  int
 	endMB    int
 }
-
-/* func AddTape() {
-	t1 := Tape{capacity: 10, readSpeed: 400, windSpeed: 600, position:0, mounted: true}
-	t1.catalog = make(map[string]TapeFile)
-	fmt.Println(t1)
-	t1.addFile(File{"f1",2000,"ds1"})
-	t1.addFile(File{"f2",2000,"ds1"})
-	t1.tapeInfo()
-} */
 
 func (t Tape) tapeInfo(){	
 	fmt.Printf("Id: %d No. of files: %d ",t.id,len(t.catalog))
