7d5f97113854ab40120d22c5fc6fc5d5e65ab7f1
Rodney.Walker <Rodney.Walker@gar-ws-etp95.garching.physik.uni-muenchen.de>
2019-02-26 16:44:13 +0100

    Add SimpleTape

---
+++ simpleTape.go
@@ -0,0 +1,60 @@
+package TapeSim
+
+import ( 
+	"fmt"
+	"sort"
+)
+
+// A fictitous 1-dimensional tape
+type SimpleTape struct {
+	Tape
+}
+
+func (t *SimpleTape) writeFile(f File) {
+	// find the last MB of the last file
+	// OR assume we do not mix read and write, so position is the end 
+
+	tf := TapeFile{t.position ,t.position+f.size}
+	t.catalog[f.fileName] = tf
+	t.position = tf.endMB
+}
+
+func (t *SimpleTape) readFile(f File) float64 {
+	//locate,seek,read
+	tf := t.catalog[f.fileName]
+	fmt.Println(tf)
+	// always going forward
+	seekTime := float64(tf.startMB - t.position) / float64(t.seekRate)
+	readTime := float64(tf.endMB - tf.startMB) / float64(t.readRate)
+	fmt.Println(seekTime,readTime,t.position)
+	// set new position
+	t.position = tf.endMB
+	return seekTime+readTime
+}
+
+func (t *SimpleTape) readFiles(f []File) float64 {
+	fmt.Println(len(f))
+	return 0.0
+}
+
+// order requested files by position
+func (t *SimpleTape) orderRequestedFiles() {
+
+type kv struct {
+	Key   string
+	Value int
+}
+
+var ss []kv
+for k, v := range t.catalog {
+	ss = append(ss, kv{k, v})
+}
+
+sort.Slice(ss, func(i, j int) bool {
+	return ss[i].Value < ss[j].Value
+})
+
+for _, kv := range ss {
+	fmt.Printf("%s, %d\n", kv.Key, kv.Value)
+}
+}
